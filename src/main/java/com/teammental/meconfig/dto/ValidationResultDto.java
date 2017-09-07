e05fe65761d73f0215963972a89c1d108cb32511
Erhan KARAKAYA <erhan277@gmail.com>
2017-09-07 11:59:52 +0300

    Added new methods

--- src/main/java/com/teammental/meconfig/dto/ValidationResultDto.java
+++ src/main/java/com/teammental/meconfig/dto/ValidationResultDto.java
@@ -1,5 +1,6 @@
 package com.teammental.meconfig.dto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ValidationResultDto {
@@ -11,8 +12,26 @@ public class ValidationResultDto {
     return fieldErrors;
   }
 
+  public ValidationResultDto() {
+    fieldErrors = new ArrayList<>();
+    globalErrors = new ArrayList<>();
+  }
+
   public void setFieldErrors(List<FieldErrorDto> fieldErrors) {
-    this.fieldErrors = fieldErrors;
+    if (fieldErrors != null) {
+      this.fieldErrors = fieldErrors;
+    }
+  }
+
+  public void addFieldError(String fieldName, String message) {
+    FieldErrorDto fieldErrorDto = new FieldErrorDto(fieldName, message);
+    fieldErrors.add(fieldErrorDto);
+  }
+
+  public void addFieldError(FieldErrorDto fieldErrorDto) {
+    if (fieldErrorDto != null) {
+      fieldErrors.add(fieldErrorDto);
+    }
   }
 
   public List<GlobalErrorDto> getGlobalErrors() {
@@ -20,6 +39,20 @@ public class ValidationResultDto {
   }
 
   public void setGlobalErrors(List<GlobalErrorDto> globalErrors) {
-    this.globalErrors = globalErrors;
+    if (globalErrors != null) {
+      this.globalErrors = globalErrors;
+    }
+  }
+
+  public void addGlobalError(String message) {
+    GlobalErrorDto globalErrorDto = new GlobalErrorDto();
+    globalErrorDto.setMessage(message);
+    globalErrors.add(globalErrorDto);
+  }
+
+  public void addGlobalError(GlobalErrorDto globalErrorDto) {
+    if(globalErrorDto != null) {
+      globalErrors.add(globalErrorDto);
+    }
   }
 }
