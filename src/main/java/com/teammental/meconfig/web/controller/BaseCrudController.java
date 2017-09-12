a270f5f0587744381570eea8b0c3f20dae5cb763
Erhan KARAKAYA <erhan277@gmail.com>
2017-09-12 17:19:11 +0300

    Fixed HttpStatus

--- src/main/java/com/teammental/meconfig/web/controller/BaseCrudController.java
+++ src/main/java/com/teammental/meconfig/web/controller/BaseCrudController.java
@@ -7,6 +7,8 @@ import com.teammental.meconfig.bll.service.BaseCrudService;
 import com.teammental.meconfig.dto.Dto;
 import com.teammental.meconfig.dto.IdDto;
 import com.teammental.meconfig.exception.NotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,36 +16,54 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 public abstract class BaseCrudController<ServiceT extends BaseCrudService, DtoT extends IdDto, IdT extends Serializable>
     extends BaseController {
 
   protected abstract ServiceT getBaseCrudService();
 
+  protected abstract String getMappingUrlOfController();
+
 
   @GetMapping("")
-  public List<DtoT> getAll() throws NotFoundException {
-    return getBaseCrudService().findAll();
+  public ResponseEntity getAll() throws NotFoundException {
+    List<DtoT> dtos = getBaseCrudService().findAll();
+    return ResponseEntity.ok(dtos);
   }
 
   @GetMapping("/{id}")
-  public DtoT getById(@PathVariable(value = "id") IdT id) throws NotFoundException {
-    return (DtoT) getBaseCrudService().findById(id);
+  public ResponseEntity getById(@PathVariable(value = "id") IdT id) throws NotFoundException {
+    DtoT dto = (DtoT) getBaseCrudService().findById(id);
+    return ResponseEntity.ok(dto);
   }
 
   @PostMapping()
-  public DtoT insert(@Validated @RequestBody DtoT dto) {
-    return (DtoT) getBaseCrudService().insert(dto);
+  public ResponseEntity insert(@Validated @RequestBody DtoT dto) {
+    dto = (DtoT) getBaseCrudService().insert(dto);
+    String location = ServletUriComponentsBuilder.fromCurrentContextPath()
+        .path(getMappingUrlOfController() + "/" + dto.getId()).build().toUriString();
+
+    return ResponseEntity
+        .status(HttpStatus.CREATED)
+        .header("Location", location)
+        .build();
   }
 
   @PutMapping()
-  public DtoT update(@Validated @RequestBody DtoT dto) throws NotFoundException {
-    return (DtoT) getBaseCrudService().update(dto);
+  public ResponseEntity update(@Validated @RequestBody DtoT dto) throws NotFoundException {
+    dto = (DtoT) getBaseCrudService().update(dto);
+    return ResponseEntity.ok(dto);
   }
 
   @DeleteMapping("/{id}")
-  public boolean delete(@PathVariable(value = "id") IdT id) throws NotFoundException {
-    return getBaseCrudService().delete(id);
+  public ResponseEntity delete(@PathVariable(value = "id") IdT id) throws NotFoundException {
+    boolean result = getBaseCrudService().delete(id);
+    if (result) {
+      return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
   }
 
 }
